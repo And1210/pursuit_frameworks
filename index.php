3ff19a8596bc1b76afd7e0b1dc14e1503f03bc47
And1210 <amf7crazy@gmail.com>
2020-04-30 16:04:54 -0400

    World View page created and styled

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
 		<link rel="stylesheet" href="assets/css/main.css">
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-		<script src="bootstrap.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/jquery-3.5.0.min.js"></script>
 	</head>
 
 	<body>
-		<a href="/"
+		<a href="/world_view.php">My World View</a>
+		<a href="/lighthouse.php">Lighthouse</a>
+		<a href="/purpose.php">Purpose</a>
+		<a href="/me.php">Me</a>
 	</body>
 
 </html>
