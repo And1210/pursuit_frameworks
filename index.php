ba07b6e1a6eb196a9b1f5a426de7479991279742
And1210 <amf7crazy@gmail.com>
2020-04-30 13:40:43 -0400

    Inital commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+	<head>
+		<title>Pursuit Inc.</title>
+
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+		<script src="bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<a href="/"
+	</body>
+
+</html>
