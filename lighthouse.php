e910c89e5eb4768ad0360a0359f5b4d08c4b52c5
And1210 <amf7crazy@gmail.com>
2020-05-02 17:36:38 -0400

    Working on lighthouse page

--- lighthouse.php
+++ lighthouse.php
@@ -13,8 +13,77 @@
 	<body>
 		<h3 align="center" class="title"><b>My Proactive Stand In The World</b></h3>
 
-		<div class="container">
+		<div class="container" align="center">
 			<hr class="title-line">
+
+			<div class="row">
+
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 d-flex flex-column">
+					<div>
+						<div class="title">Space:</div>
+						<div class="form-group flex-grow-1 d-flex flex-column">
+							<textarea class="form-control flex-grow-1	black"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 d-flex flex-column">
+					<div style="position: relative; width: 120%; left: -10%;">
+						<div class="title">Show-Up:</div>
+						<div class="form-group flex-grow-1 d-flex flex-column">
+							<textarea class="form-control flex-grow-1	black"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 d-flex flex-column">
+					<div style="position: relative; width: 140%; left: -20%;">
+						<div class="title">My Contribution:</div>
+						<div class="form-group flex-grow-1 d-flex flex-column">
+							<textarea class="form-control flex-grow-1	black"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 d-flex flex-column">
+					<div style="position: relative; width: 160%; left: -30%;">
+						<div class="title">Meaningful To Me Because (Impact I Desire):</div>
+						<div class="form-group flex-grow-1 d-flex flex-column">
+							<textarea class="form-control flex-grow-1	black"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 d-flex flex-column">
+					<div style="position: relative; width: 180%; left: -40%;">
+						<div class="title">Meaningful Experience:</div>
+						<div class="form-group flex-grow-1 d-flex flex-column">
+							<textarea class="form-control flex-grow-1	black"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
 		</div>
 	</body>
 
--- world_view.php
+++ world_view.php
@@ -15,45 +15,45 @@
 
 		<div class="container">
 			<hr class="title-line">
-			
+
 			<!-- Title Row 1 -->
 			<div class="row" align="center">
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<h6 style="color: red;">(-) Burns Energy</h6>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<h6 style="color: blue;">(+) Builds Energy</h6>
 				</div>
 			</div>
 			<div class="row" align="center">
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">Feel</h6>
 				</div>
-				<div class="col-sm-8">
+				<div class="col-md-8">
 					<h6 class="subtitle">The Big World</h6>
 				</div>
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">Feel</h6>
 				</div>
 			</div>
 
 			<!-- Textarea Row 1 -->
 			<div class="row">
-				<div class="col-sm-2 d-flex flex-column">
+				<div class="col-md-2 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea class="form-control flex-grow-1	black"></textarea>
 					</div>
 				</div>
 
 				<!-- Textarea Row 1a -->
-				<div class="col-sm-8">
+				<div class="col-md-8">
 					<div class="row">
-						<div class="col-sm-6 d-flex flex-column">
+						<div class="col-md-6 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	black"></textarea>
 							</div>
 						</div>
-						<div class="col-sm-6 d-flex flex-column">
+						<div class="col-md-6 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	black"></textarea>
 							</div>
@@ -62,30 +62,30 @@
 
 					<h6 align="center" class="subtitle">My World - Communities</h6> <!-- Title Row 1a -->
 					<div class="row">
-						<div class="col-sm-6">
+						<div class="col-md-6">
 							<div class="row">
-								<div class="col-sm-1"></div>
-								<div class="col-sm-11 d-flex flex-column">
+								<div class="col-md-1"></div>
+								<div class="col-md-11 d-flex flex-column">
 									<div class="form-group flex-grow-1 d-flex flex-column">
 										<textarea class="form-control flex-grow-1	blue"></textarea>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="col-sm-6">
+						<div class="col-md-6">
 							<div class="row">
-								<div class="col-sm-11 d-flex flex-column">
+								<div class="col-md-11 d-flex flex-column">
 									<div class="form-group flex-grow-1 d-flex flex-column">
 										<textarea class="form-control flex-grow-1	blue"></textarea>
 									</div>
 								</div>
-								<div class="col-sm-1"></div>
+								<div class="col-md-1"></div>
 							</div>
 						</div>
 					</div>
 				</div>
 
-				<div class="col-sm-2 d-flex flex-column">
+				<div class="col-md-2 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea class="form-control flex-grow-1	black"></textarea>
 					</div>
@@ -94,43 +94,43 @@
 
 			<!-- Title Row 2 -->
 			<div class="row" align="center">
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">React</h6>
 				</div>
-				<div class="col-sm-8">
+				<div class="col-md-8">
 					<h6 class="subtitle">My Work World</h6>
 				</div>
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">Respond</h6>
 				</div>
 			</div>
 
 			<!-- Textarea Row 2 -->
 			<div class="row">
-				<div class="col-sm-2 d-flex flex-column">
+				<div class="col-md-2 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea style="width: 135%;" class="form-control flex-grow-1	black"></textarea>
 					</div>
 				</div>
 
-				<div class="col-sm-8">
+				<div class="col-md-8">
 					<div class="row">
-						<div class="col-sm-1"></div>
-						<div class="col-sm-5 d-flex flex-column">
+						<div class="col-md-1"></div>
+						<div class="col-md-5 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	red"></textarea>
 							</div>
 						</div>
-						<div class="col-sm-5 d-flex flex-column">
+						<div class="col-md-5 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	red"></textarea>
 							</div>
 						</div>
-						<div class="col-sm-1"></div>
+						<div class="col-md-1"></div>
 					</div>
 				</div>
 
-				<div class="col-sm-2 d-flex flex-column">
+				<div class="col-md-2 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea style="width: 135%; position: relative; left: -35%;" class="form-control flex-grow-1	black"></textarea>
 					</div>
@@ -139,33 +139,33 @@
 
 			<!-- Title Row 3 -->
 			<div class="row" align="center">
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">Impact</h6>
 				</div>
-				<div class="col-sm-8">
+				<div class="col-md-8">
 					<h6 class="subtitle">My Personal World</h6>
 				</div>
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<h6 class="subtitle">Impact</h6>
 				</div>
 			</div>
 
 			<!-- Textarea Row 3 -->
 			<div class="row">
-				<div class="col-sm-3 d-flex flex-column">
+				<div class="col-md-3 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea class="form-control flex-grow-1	black" id="impact_neg">Me: &#13;&#10;Others: &#13;&#10;World: </textarea>
 					</div>
 				</div>
 
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<div class="row">
-						<div class="col-sm-6 d-flex flex-column">
+						<div class="col-md-6 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	green"></textarea>
 							</div>
 						</div>
-						<div class="col-sm-6 d-flex flex-column">
+						<div class="col-md-6 d-flex flex-column">
 							<div class="form-group flex-grow-1 d-flex flex-column">
 								<textarea class="form-control flex-grow-1	green"></textarea>
 							</div>
@@ -173,7 +173,7 @@
 					</div>
 				</div>
 
-				<div class="col-sm-3 d-flex flex-column">
+				<div class="col-md-3 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea class="form-control flex-grow-1	black" id="impact_pos">Me: &#13;&#10;Others: &#13;&#10;World: </textarea>
 					</div>
@@ -182,40 +182,40 @@
 
 			<!-- Title Row 4 -->
 			<div class="row" align="center">
-				<div class="col-sm-1"></div>
-				<div class="col-sm-4">
+				<div class="col-md-1"></div>
+				<div class="col-md-4">
 					<h6 class="subtitle">Position</h6>
 				</div>
-				<div class="col-sm-2"></div>
-				<div class="col-sm-4">
+				<div class="col-md-2"></div>
+				<div class="col-md-4">
 					<h6 class="subtitle">Position</h6>
 				</div>
-				<div class="col-sm-1"></div>
+				<div class="col-md-1"></div>
 			</div>
 
 			<!-- Textarea Row 4 -->
 			<div class="row">
-				<div class="col-sm-1"></div>
-				<div class="col-sm-4 d-flex flex-column">
+				<div class="col-md-1"></div>
+				<div class="col-md-4 d-flex flex-column">
 					<input type="text" class="black">
 				</div>
-				<div class="col-sm-2" align="center">
+				<div class="col-md-2" align="center">
 					<img src="assets/img/globe.png" height=50>
 				</div>
-				<div class="col-sm-4 d-flex flex-column">
+				<div class="col-md-4 d-flex flex-column">
 					<input type="text" class="black">
 				</div>
-				<div class="col-sm-1"></div>
+				<div class="col-md-1"></div>
 			</div>
 
 			<div class="row" style="padding-top: 20px;">
-				<div class="col-sm-3"></div>
-				<div class="col-sm-6 d-flex flex-column">
+				<div class="col-md-3"></div>
+				<div class="col-md-6 d-flex flex-column">
 					<div class="form-group flex-grow-1 d-flex flex-column">
 						<textarea class="form-control flex-grow-1 small-input	grey"></textarea>
 					</div>
 				</div>
-				<div class="col-sm-3"></div>
+				<div class="col-md-3"></div>
 			</div>
 		</div>
 	</body>
@@ -224,11 +224,11 @@
 		<div class="container">
 			<hr class="footer-line">
 			<div class="row">
-				<div class="col-sm-2">
+				<div class="col-md-2">
 					<img src="assets/img/logo.png" height=40>
 				</div>
-				<div class="col-sm-2"></div>
-				<div class="col-sm-8">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
 					<p class="copyright"><br>Copyright &copy; 2007-2020 Pursuit
 						Development Labs Inc. All Rights Reserved.</p>
 				</div>
